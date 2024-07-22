91b57f54e833864d2b1cbca5b5c9aa31f1456266
Guilherme Gustavo Correia <CS404937@agfips.com.br>
2024-07-22 11:22:00 -0300

    corrigindo erro css pagina admin

--- setup/settings.py
+++ setup/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -150,12 +150,12 @@ TEMPLATES = [
 
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 MEDIA_URL  = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "previsao_trens/media/")
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
+
 
 LOGIN_REDIRECT_URL = '/navegacao'
 
