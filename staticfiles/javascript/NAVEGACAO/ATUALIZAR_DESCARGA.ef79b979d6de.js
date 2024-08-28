fc01b6d1c9f1421f27d7f32ed91548977c3e755a
Guilherme Gustavo <guilherme_gustavo.2808@agfips.com.br>
2024-08-28 16:02:37 -0300

    ajuste dividir trem encoste com alerta com debug - False

---
+++ staticfiles/javascript/NAVEGACAO/ATUALIZAR_DESCARGA.ef79b979d6de.js
@@ -0,0 +1,189 @@
+
+function corrigirSintaxeJSON(stringComErro) {
+    // Remover apóstrofos desnecessários das chaves e nomes de string
+    let stringCorrigida = stringComErro
+        .replace(/'/g, '"')  // Substitui todos os apóstrofos por aspas duplas
+        .replace(/([{,]\s*)([A-Za-z0-9_]+)\s*:/g, '$1"$2":');  // Adiciona aspas duplas em nomes de chave
+
+    return stringCorrigida;
+}
+function LIMPAR_SELECOES(){
+
+
+    var CELULAS_SELECIONADAS    = document.querySelectorAll('.CELULA_SELECIONADA');
+    var CELULAS_PRIMEIRA        = document.querySelectorAll('.PRIMEIRA');
+    var CELULAS_ULTIMA          = document.querySelectorAll('.ULTIMA');
+    
+    CELULAS_PRIMEIRA.forEach(function(CELULA)       {CELULA.classList.remove('PRIMEIRA');});
+    CELULAS_ULTIMA.forEach(function(CELULA)         {CELULA.classList.remove('ULTIMA');});
+    CELULAS_SELECIONADAS.forEach(function(CELULA)   {CELULA.classList.remove('CELULA_SELECIONADA');});
+
+    let LINHA_PRODUTIVIDADE = document.getElementById('LINHA_EM_EDICAO')
+    if (LINHA_PRODUTIVIDADE !== null) {LINHA_PRODUTIVIDADE.removeAttribute('id')}
+
+    NOVO_VALOR         = ""
+}
+
+function ATUALIZAR_DESCARGA(DESCARGAS){
+
+    for (let i = 0; i < DESCARGAS.length; i++){
+        
+        LIMPAR_SELECOES()
+
+        let TERMINAL        = DESCARGAS[i]["TERMINAL"]
+        let DATA_ARQ        = DESCARGAS[i]["DATA"]
+        let TABELA_DESCARGA = document.getElementById(`${ TERMINAL }_${ DATA_ARQ }`)
+       
+        // SEXTA 13:45 
+
+        if(TABELA_DESCARGA){
+
+            let DESCARGAS_ATIVAS = JSON.parse(corrigirSintaxeJSON(TABELA_DESCARGA.querySelector(`#DESCARGAS_ATIVAS`).value))
+        
+            //#region REMOVENDO O QUE NAO ESTA ATIVO
+            for (let CHAVE in DESCARGAS_ATIVAS) {
+                if (DESCARGAS_ATIVAS[CHAVE].length == 0){ delete DESCARGAS[i]["DESCARGAS"][CHAVE]; delete DESCARGAS_ATIVAS[CHAVE];}
+            }
+            //#endregion
+
+            //#region INSERINDO A LINHA DA PEDRA
+            for (let k = 0; k < 24; k++){
+                let FILTRO_PEDRA         = `td[headers="${k}"][name="LINHA_PEDRA"]`
+
+                let ELEMENTO_PEDRA       = TABELA_DESCARGA.querySelector(FILTRO_PEDRA);
+                if (Number(DESCARGAS[i]["PEDRA"][k]) !== 0) {ELEMENTO_PEDRA.innerText = DESCARGAS[i]["PEDRA"][k]}
+                else                                        {ELEMENTO_PEDRA.innerText = ""}
+            }
+            //#endregion
+      
+            //#region INSERINDO TOTAIS
+
+            let TOTAL_SALDO = TABELA_DESCARGA.querySelector("#TOTAL_SALDO")
+            let TOTAL_PEDRA = TABELA_DESCARGA.querySelector("#TOTAL_PEDRA")
+            
+            TOTAL_SALDO.innerText = DESCARGAS[i]["INDICADORES"]["TOTAL_SALDO"]
+            TOTAL_PEDRA.innerText = DESCARGAS[i]["INDICADORES"]["TOTAL_PEDRA"]
+
+            let PEDRAS = {
+                "RUMO":  {"P1": 0, "P2": 0, "P3": 0, "P4": 0},
+                "MRS":   {"P1": 0, "P2": 0, "P3": 0, "P4": 0},
+                "VLI":   {"P1": 0, "P2": 0, "P3": 0, "P4": 0},
+                "TOTAL": {"P1": 0, "P2": 0, "P3": 0, "P4": 0}
+            }
+
+            let ELEMENTO_RUMO_P1       = TABELA_DESCARGA.querySelector(`td[data-periodo="1"].TOTAL_FERROVIA_RUMO`);
+            let ELEMENTO_RUMO_P2       = TABELA_DESCARGA.querySelector(`td[data-periodo="2"].TOTAL_FERROVIA_RUMO`);
+            let ELEMENTO_RUMO_P3       = TABELA_DESCARGA.querySelector(`td[data-periodo="3"].TOTAL_FERROVIA_RUMO`);
+            let ELEMENTO_RUMO_P4       = TABELA_DESCARGA.querySelector(`td[data-periodo="4"].TOTAL_FERROVIA_RUMO`);
+
+            let ELEMENTO_MRS_P1       = TABELA_DESCARGA.querySelector(`td[data-periodo="1"].TOTAL_FERROVIA_MRS`);
+            let ELEMENTO_MRS_P2       = TABELA_DESCARGA.querySelector(`td[data-periodo="2"].TOTAL_FERROVIA_MRS`);
+            let ELEMENTO_MRS_P3       = TABELA_DESCARGA.querySelector(`td[data-periodo="3"].TOTAL_FERROVIA_MRS`);
+            let ELEMENTO_MRS_P4       = TABELA_DESCARGA.querySelector(`td[data-periodo="4"].TOTAL_FERROVIA_MRS`);
+
+            let ELEMENTO_VLI_P1       = TABELA_DESCARGA.querySelector(`td[data-periodo="1"].TOTAL_FERROVIA_VLI`);
+            let ELEMENTO_VLI_P2       = TABELA_DESCARGA.querySelector(`td[data-periodo="2"].TOTAL_FERROVIA_VLI`);
+            let ELEMENTO_VLI_P3       = TABELA_DESCARGA.querySelector(`td[data-periodo="3"].TOTAL_FERROVIA_VLI`);
+            let ELEMENTO_VLI_P4       = TABELA_DESCARGA.querySelector(`td[data-periodo="4"].TOTAL_FERROVIA_VLI`);
+
+            let ELEMENTO_TOTAIS_P1       = TABELA_DESCARGA.querySelector(`td[data-periodo="1"].TITLO_TOTAIS`);
+            let ELEMENTO_TOTAIS_P2       = TABELA_DESCARGA.querySelector(`td[data-periodo="2"].TITLO_TOTAIS`);
+            let ELEMENTO_TOTAIS_P3       = TABELA_DESCARGA.querySelector(`td[data-periodo="3"].TITLO_TOTAIS`);
+            let ELEMENTO_TOTAIS_P4       = TABELA_DESCARGA.querySelector(`td[data-periodo="4"].TITLO_TOTAIS`);
+
+            ELEMENTO_RUMO_P1.innerText = DESCARGAS[i]["INDICADORES"]["PEDRAS"]["RUMO"]["P1"]
+            ELEMENTO_RUMO_P2.innerText = DESCARGAS[i]["INDICADORES"]["PEDRAS"]["RUMO"]["P2"]
+            ELEMENTO_RUMO_P3.innerText = DESCARGAS[i]["INDICADORES"]["PEDRAS"]["RUMO"]["P3"]
+            ELEMENTO_RUMO_P4.innerText = DESCARGAS[i]["INDICADORES"]["PEDRAS"]["RUMO"]["P4"]
+            
+            ELEMENTO_MRS_P1.innerText = DESCARGAS[i]["INDICADORES"]["PEDRAS"]["MRS"]["P1"]
+            ELEMENTO_MRS_P2.innerText = DESCARGAS[i]["INDICADORES"]["PEDRAS"]["MRS"]["P2"]
+            ELEMENTO_MRS_P3.innerText = DESCARGAS[i]["INDICADORES"]["PEDRAS"]["MRS"]["P3"]
+            ELEMENTO_MRS_P4.innerText = DESCARGAS[i]["INDICADORES"]["PEDRAS"]["MRS"]["P4"]
+            
+            ELEMENTO_VLI_P1.innerText = DESCARGAS[i]["INDICADORES"]["PEDRAS"]["VLI"]["P1"]
+            ELEMENTO_VLI_P2.innerText = DESCARGAS[i]["INDICADORES"]["PEDRAS"]["VLI"]["P2"]
+            ELEMENTO_VLI_P3.innerText = DESCARGAS[i]["INDICADORES"]["PEDRAS"]["VLI"]["P3"]
+            ELEMENTO_VLI_P4.innerText = DESCARGAS[i]["INDICADORES"]["PEDRAS"]["VLI"]["P4"]
+            
+            ELEMENTO_TOTAIS_P1.innerText = DESCARGAS[i]["INDICADORES"]["PEDRAS"]["TOTAL"]["P1"]
+            ELEMENTO_TOTAIS_P2.innerText = DESCARGAS[i]["INDICADORES"]["PEDRAS"]["TOTAL"]["P2"]
+            ELEMENTO_TOTAIS_P3.innerText = DESCARGAS[i]["INDICADORES"]["PEDRAS"]["TOTAL"]["P3"]
+            ELEMENTO_TOTAIS_P4.innerText = DESCARGAS[i]["INDICADORES"]["PEDRAS"]["TOTAL"]["P4"]
+
+            //#endregion
+
+            for (let FERROVIA in DESCARGAS_ATIVAS){
+                
+                for (let j = 0; j < DESCARGAS_ATIVAS[FERROVIA].length; j++){
+                    
+                    let PRODUTO = DESCARGAS_ATIVAS[FERROVIA][j]
+                    
+                    //#region INSERINDO SALDO DE VIRADA
+
+                    let FILTRO_SALDO_VIRADA         = `td[data-ferrovia="${FERROVIA}"][data-produto="${PRODUTO}"][headers="SALDO_VIRADA"]`
+
+                    let ELEMENTO_SALDO_VIRADA       = TABELA_DESCARGA.querySelector(FILTRO_SALDO_VIRADA);
+
+                    ELEMENTO_SALDO_VIRADA.innerText = DESCARGAS[i]["DESCARGAS"][FERROVIA][PRODUTO]["INDICADORES"]["SALDO_DE_VIRADA"]
+
+                    //#endregion
+
+                    //#region INSERINDO TOTAL PRODUTIVIDADE 
+
+                    let FILTRO_TOTAL_PRODUTIVIDADE  = `td[data-ferrovia="${FERROVIA}"][data-produto="${PRODUTO}"][name="TOTAL_PRODUTIVIDADE"]`
+
+                    let ELEMENTO_TOTAL_PRODUTIVIDADE       = TABELA_DESCARGA.querySelector(FILTRO_TOTAL_PRODUTIVIDADE);
+           
+                    ELEMENTO_TOTAL_PRODUTIVIDADE.innerText = DESCARGAS[i]["DESCARGAS"][FERROVIA][PRODUTO]["INDICADORES"]["TOTAL_PRODUTIVIDADE"]
+
+                    //#endregion
+
+                    for (let k = 0; k < 24; k++){
+
+                        //#region INSERINDO A LINHA DA PRODUTIVIDADE
+
+                        let FILTRO_PRODUTIVIDADE         = `td[data-ferrovia="${FERROVIA}"][data-produto="${PRODUTO}"][headers="${k}"][name="PRODUTIVIDADE"]`
+
+                        let ELEMENTO_PRODUTIVIDADE       = TABELA_DESCARGA.querySelector(FILTRO_PRODUTIVIDADE);
+                        if (Number(DESCARGAS[i]["DESCARGAS"][FERROVIA][PRODUTO]["PRODUTIVIDADE"][k]) !== 0) {ELEMENTO_PRODUTIVIDADE.innerText = DESCARGAS[i]["DESCARGAS"][FERROVIA][PRODUTO]["PRODUTIVIDADE"][k]}
+                        else                                                                                {ELEMENTO_PRODUTIVIDADE.innerText = ""}
+                        //#endregion
+
+                        //#region INSERINDO A LINHA DO SALDO
+
+                        let FILTRO_SALDO         = `td[data-ferrovia="${FERROVIA}"][data-produto="${PRODUTO}"][headers="${k}"][name="SALDO"]`
+
+                        let ELEMENTO_SALDO       = TABELA_DESCARGA.querySelector(FILTRO_SALDO);
+                        if (Number(DESCARGAS[i]["DESCARGAS"][FERROVIA][PRODUTO]["SALDO"][k]) !== 0)
+                        {ELEMENTO_SALDO.innerText = DESCARGAS[i]["DESCARGAS"][FERROVIA][PRODUTO]["SALDO"][k]}
+                        else                                        {ELEMENTO_SALDO.innerText = ""}
+                        //#endregion
+
+                        //#region INSERINDO A LINHA DO ENCOSTE
+                        let FILTRO_ENCOSTE       = `td[data-ferrovia="${FERROVIA}"][data-mercadoria="${PRODUTO}"][headers="${k}"][name="encoste"]`
+                        let ELEMENTO_ENCOSTE       = TABELA_DESCARGA.querySelector(FILTRO_ENCOSTE);
+                        if (Number(DESCARGAS[i]["DESCARGAS"][FERROVIA][PRODUTO]["ENCOSTE"][k][0]) !== 0){
+                            
+                            if (Number(DESCARGAS[i]["DESCARGAS"][FERROVIA][PRODUTO]["ENCOSTE"][k][1]) == 0){
+                                ELEMENTO_ENCOSTE.classList.add("encoste_virtual")
+                            }
+                            else{
+                                ELEMENTO_ENCOSTE.classList.remove("encoste_virtual") 
+                            }
+                            ELEMENTO_ENCOSTE.innerText = DESCARGAS[i]["DESCARGAS"][FERROVIA][PRODUTO]["ENCOSTE"][k][0]
+
+                        }
+                        
+                        else {
+                            ELEMENTO_ENCOSTE.classList.remove("encoste_virtual") 
+                            ELEMENTO_ENCOSTE.innerText = ""
+                        }
+                        //#endregion
+                    
+                    }
+                }
+            }      
+        }    
+    }
+}
---
+++ staticfiles/javascript/NAVEGACAO/EDITAR_PRODUTIVIDADE.07391ba48002.js
@@ -0,0 +1,408 @@
+var MOUSE_PRESSIONADO   = false
+var MODO_EDICAO         = false
+var EDITAR_SALDO_VIRADA = false
+var EDITAR_PRODUTIVIADE = false
+var NOVO_VALOR_SALDO = ""
+var NOVO_VALOR          = ""
+
+var PARAMETROS_EDITADOS = {
+    "TERMINAL": "",
+    "DATA_ARQ": "",
+    "FERROVIA": "",
+    "PRODUTO" : "",
+    "CELULAS" : [],
+    "VALOR"   : 0
+}
+
+let SENTIDO_SELECAO = ""
+let editando_encoste = false
+let NOVA_SELECAO = []
+let params_encoste = {}
+
+//#region PARA AO ATUALIZAR NAO VOLTAR AO INICIO DA PAGINA
+
+window.addEventListener('load', function() {
+
+    const conteudoNavegacao = document.getElementById('conteudo__navegacao');
+
+    // Checking if there's a saved scroll position
+    if (localStorage.getItem('scrollX') && localStorage.getItem('scrollY')) {
+
+        conteudoNavegacao.scrollLeft = localStorage.getItem('scrollX');
+        conteudoNavegacao.scrollTop = localStorage.getItem('scrollY');
+        
+    }
+
+    // Save scroll position before page unloads
+    window.addEventListener('beforeunload', function() {
+
+        localStorage.setItem('scrollX', conteudoNavegacao.scrollLeft);
+        localStorage.setItem('scrollY', conteudoNavegacao.scrollTop);
+
+    });
+
+});
+
+//#endregion
+
+//#region FUNCOES INTERNAS
+function LIMPAR_SELECOES(){
+
+    MODO_EDICAO = false
+    EDITAR_SALDO_VIRADA = false;
+    var CELULAS_SELECIONADAS    = document.querySelectorAll('.CELULA_SELECIONADA');
+    var CELULAS_PRIMEIRA        = document.querySelectorAll('.PRIMEIRA');
+    var CELULAS_ULTIMA          = document.querySelectorAll('.ULTIMA');
+    
+    CELULAS_PRIMEIRA.forEach(function(CELULA)       {CELULA.classList.remove('PRIMEIRA');});
+    CELULAS_ULTIMA.forEach(function(CELULA)         {CELULA.classList.remove('ULTIMA');});
+    CELULAS_SELECIONADAS.forEach(function(CELULA)   {CELULA.classList.remove('CELULA_SELECIONADA');});
+
+    let LINHA_PRODUTIVIDADE = document.getElementById('LINHA_EM_EDICAO')
+    if (LINHA_PRODUTIVIDADE !== null) {LINHA_PRODUTIVIDADE.removeAttribute('id')}
+
+    NOVO_VALOR         = ""
+}
+
+function DESENHAR_BORDA(CELULAS_INCICES){
+    
+    CELULAS_INCICES.forEach(function(INDICE) {
+        
+        let CELULA = document.querySelector(`#LINHA_EM_EDICAO td[headers="${ INDICE }"]` )
+
+        if (CELULA) {
+
+            CELULA.classList.remove('CELULA_SELECIONADA');
+            CELULA.classList.remove('PRIMEIRA');
+            CELULA.classList.remove('ULTIMA');
+
+        } 
+
+    });
+
+    CELULAS_INCICES.forEach(function(INDICE) {
+        let CELULA = document.querySelector(`#LINHA_EM_EDICAO td[headers="${ INDICE }"]` )
+        if (CELULA) {CELULA.classList.add('CELULA_SELECIONADA');} 
+    });
+
+    let INDICE_PRIMEIRA = Math.min(...CELULAS_INCICES);
+    let PRIMEIRA_CELULA = document.querySelector(`#LINHA_EM_EDICAO td[headers="${ INDICE_PRIMEIRA }"]` )
+
+
+    let INDICE_ULTIMA = Math.max(...CELULAS_INCICES);
+    let ULTIMA_CELULA = document.querySelector(`#LINHA_EM_EDICAO td[headers="${ INDICE_ULTIMA }"]` )
+    
+    try{
+        
+        PRIMEIRA_CELULA.classList.add('PRIMEIRA');
+        ULTIMA_CELULA.classList.add('ULTIMA');
+
+    }
+    catch{
+
+        return
+
+    }
+}
+
+class mudarElemento {
+
+    constructor(elemento)   {
+  
+        this.elemento       = elemento
+        this.valor_original = elemento.innerText
+        this.posicao_coluna = elemento.cellIndex
+    }
+  
+    para_textBox()          {
+        
+        var textBox = document.createElement('input');
+        
+        textBox.className   = 'input_encoste';
+        textBox.id          = this.elemento.id;
+        textBox.type        = 'text';
+        textBox.placeholder = this.valor_original;
+        
+        this.elemento.innerHTML = '';
+        this.elemento.appendChild(textBox);
+  
+        return textBox
+  
+    }
+
+    voltar_ao_nomral()      {
+
+        this.elemento.innerHTML = this.valor_original;
+    }
+  
+}
+//#endregion
+
+let ctrlPressed = false
+
+document.body.addEventListener('mousedown', async function(event) {
+   
+    MOUSE_PRESSIONADO = true
+    let CELULA_SELECIONADA = event.target;
+
+    let LINHA_PRODUTIVIDADE = CELULA_SELECIONADA.parentNode
+
+    EDITAR_SALDO_VIRADA = false
+    EDITAR_PRODUTIVIADE = false
+
+    //CLICANDO NA CÉLULA DE PRODUTIVIDADE   
+    if (CELULA_SELECIONADA.getAttribute('name') === "PRODUTIVIDADE" && ctrlPressed === false){
+
+        LIMPAR_SELECOES()
+
+        MODO_EDICAO = true
+        let ID_TABELA = LINHA_PRODUTIVIDADE.parentNode.parentNode.id.split("_")
+        let FERROVIA_PRODUTO = LINHA_PRODUTIVIDADE.getElementsByTagName("td")[1].id.split("_")
+
+        PARAMETROS_EDITADOS["TERMINAL"] = ID_TABELA[0]
+        PARAMETROS_EDITADOS["DATA_ARQ"] = ID_TABELA[1]
+
+        PARAMETROS_EDITADOS["PRODUTO"]  = FERROVIA_PRODUTO[0]
+        PARAMETROS_EDITADOS["FERROVIA"] = FERROVIA_PRODUTO[1]
+        
+        PARAMETROS_EDITADOS["CELULAS"] = []
+        PARAMETROS_EDITADOS["CELULAS"][0] = CELULA_SELECIONADA.getAttribute('headers')
+
+        NOVA_SELECAO = [CELULA_SELECIONADA.getAttribute('headers')]
+        
+        LINHA_PRODUTIVIDADE.id = 'LINHA_EM_EDICAO';  
+        
+        DESENHAR_BORDA(NOVA_SELECAO)
+
+    }
+
+    else if(CELULA_SELECIONADA.getAttribute('name') === "PRODUTIVIDADE" && ctrlPressed === true){
+
+        let POSICAO_CELULA = CELULA_SELECIONADA.getAttribute('headers')
+        PARAMETROS_EDITADOS["CELULAS"].push(POSICAO_CELULA);
+
+
+        NOVA_SELECAO = [POSICAO_CELULA];
+        DESENHAR_BORDA(NOVA_SELECAO)
+
+    }
+
+    else if(CELULA_SELECIONADA.getAttribute('name') === "SALDO_DE_VIRADA_D"){
+        
+        MODO_EDICAO = false
+        EDITAR_SALDO_VIRADA = true
+        NOVO_VALOR_SALDO = ""
+        
+        LIMPAR_SELECOES()
+        DESENHAR_BORDA(PARAMETROS_EDITADOS["CELULAS"])
+
+        let ID_TABELA           = CELULA_SELECIONADA.parentNode.parentNode.parentNode.id.split("_")
+
+        PARAMETROS_EDITADOS["TERMINAL"] = ID_TABELA[0]
+        PARAMETROS_EDITADOS["DATA_ARQ"] = ID_TABELA[1]
+
+        PARAMETROS_EDITADOS["PRODUTO"]  = CELULA_SELECIONADA.dataset.produto
+        PARAMETROS_EDITADOS["FERROVIA"] = CELULA_SELECIONADA.dataset.ferrovia
+        
+        PARAMETROS_EDITADOS["CELULAS"][0] = ""
+
+        CELULA_SELECIONADA.classList.add('CELULA_SELECIONADA');
+        CELULA_SELECIONADA.classList.add('ULTIMA');
+        CELULA_SELECIONADA.classList.add('PRIMEIRA');
+  
+    }
+
+    else if(CELULA_SELECIONADA.getAttribute('name') === "EDITAR_CONSTANTE_PRODUTIVIDADE"){
+        
+        EDITAR_PRODUTIVIADE = true
+        NOVO_VALOR_SALDO = ""
+
+        LIMPAR_SELECOES()
+        DESENHAR_BORDA(PARAMETROS_EDITADOS["CELULAS"])
+
+        let ID_TABELA           = CELULA_SELECIONADA.parentNode.parentNode.parentNode.id.split("_")
+
+        PARAMETROS_EDITADOS["TERMINAL"] = ID_TABELA[0]
+        PARAMETROS_EDITADOS["DATA_ARQ"] = ID_TABELA[1]
+
+        PARAMETROS_EDITADOS["PRODUTO"]  = CELULA_SELECIONADA.dataset.produto
+        PARAMETROS_EDITADOS["FERROVIA"] = CELULA_SELECIONADA.dataset.ferrovia
+
+        PARAMETROS_EDITADOS["CELULAS"][0] = ""
+
+        CELULA_SELECIONADA.classList.add('CELULA_SELECIONADA');
+        CELULA_SELECIONADA.classList.add('ULTIMA');
+        CELULA_SELECIONADA.classList.add('PRIMEIRA');
+        
+    }
+
+    else{ 
+
+        LIMPAR_SELECOES(); 
+        MODO_EDICAO = false; 
+        DESENHAR_BORDA(PARAMETROS_EDITADOS["CELULAS"]) 
+
+    }   //NÃO É CÉLULA DE PRODUTIVIDADE
+
+})
+
+document.body.addEventListener('mouseover', async function(event) {
+    
+    let CELULA_SELECIONADA  = event.target;
+    let LINHA_PRODUTIVIDADE = CELULA_SELECIONADA.parentNode
+    let ESTA_NA_MESMA_LINHA = document.getElementById('LINHA_EM_EDICAO') === LINHA_PRODUTIVIDADE 
+        
+    let CELULA_DE_PRODUTIVIDADE = CELULA_SELECIONADA.getAttribute('name') === "PRODUTIVIDADE"
+
+    if(MOUSE_PRESSIONADO && MODO_EDICAO && ESTA_NA_MESMA_LINHA && CELULA_DE_PRODUTIVIDADE){
+        
+        let POSICAO_CELULA = CELULA_SELECIONADA.getAttribute('headers')
+
+        //DEFININDO O SENTIDO DA SELEÇÃO [ESQUERDA OU DIREITA]
+        if (PARAMETROS_EDITADOS["CELULAS"].length === 1){
+            
+            if (PARAMETROS_EDITADOS["CELULAS"][0] >  POSICAO_CELULA) {SENTIDO_SELECAO = "ESQUERDA";}
+            else                                                     {SENTIDO_SELECAO = "DIREITA"; }
+        }
+        
+        if (SENTIDO_SELECAO === `DIREITA`){
+
+            if (POSICAO_CELULA == Math.max(...NOVA_SELECAO) + 1){
+
+                PARAMETROS_EDITADOS["CELULAS"].push(POSICAO_CELULA);
+                NOVA_SELECAO.push(POSICAO_CELULA)
+
+                DESENHAR_BORDA(NOVA_SELECAO)
+            }
+
+
+            if (POSICAO_CELULA == Math.max(...NOVA_SELECAO) - 1){ //REMOVER ULTIMA CELULA POIS VOLTAMOS UMA CÉLULA PARA TRÁS
+            
+
+                var POSICAO_ULTIMA_CELULA = Math.max(...NOVA_SELECAO);
+                
+                let POSICAO_ULTIMA_CELULA_PARAM = PARAMETROS_EDITADOS["CELULAS"].indexOf(POSICAO_ULTIMA_CELULA);
+                let POSICAO_ULTIMA_CELULA_NOVA  = NOVA_SELECAO.indexOf(POSICAO_ULTIMA_CELULA);
+
+                //REMOVENDO ITEM DA LISTA  
+                NOVA_SELECAO.splice(POSICAO_ULTIMA_CELULA_NOVA, 1); 
+                PARAMETROS_EDITADOS["CELULAS"].splice(POSICAO_ULTIMA_CELULA_PARAM, 1); 
+
+                //REMOVENDO BORDA 
+                let ULTIMA_CELULA = document.querySelector(`#LINHA_EM_EDICAO td[headers="${ POSICAO_ULTIMA_CELULA }"]` )
+                ULTIMA_CELULA.classList.remove('CELULA_SELECIONADA');
+                ULTIMA_CELULA.classList.remove('ULTIMA');
+
+                DESENHAR_BORDA(NOVA_SELECAO)
+            }
+        
+        }
+
+        if(SENTIDO_SELECAO === `ESQUERDA`){
+
+            if (POSICAO_CELULA == Math.min(...PARAMETROS_EDITADOS["CELULAS"]) - 1){
+    
+
+                PARAMETROS_EDITADOS["CELULAS"].push(POSICAO_CELULA);
+                DESENHAR_BORDA(PARAMETROS_EDITADOS["CELULAS"])
+
+            }
+            if (POSICAO_CELULA == Math.min(...PARAMETROS_EDITADOS["CELULAS"]) + 1){
+            
+                var POSICAO_ULTIMA_CELULA =  Math.min(...PARAMETROS_EDITADOS["CELULAS"]);
+
+                let POSICAO_ULTIMA_CELULA_PARAM = PARAMETROS_EDITADOS["CELULAS"].indexOf(POSICAO_ULTIMA_CELULA);
+                let POSICAO_ULTIMA_CELULA_NOVA  = NOVA_SELECAO.indexOf(POSICAO_ULTIMA_CELULA);
+
+                //REMOVENDO ITEM DA LISTA  
+                NOVA_SELECAO.splice(POSICAO_ULTIMA_CELULA_NOVA, 1); 
+                PARAMETROS_EDITADOS["CELULAS"].splice(POSICAO_ULTIMA_CELULA_PARAM, 1); 
+
+                //REMOVENDO BORDA 
+                let ULTIMA_CELULA = document.querySelector(`#LINHA_EM_EDICAO td[headers="${ POSICAO_ULTIMA_CELULA }"]` )
+                ULTIMA_CELULA.classList.remove('CELULA_SELECIONADA');
+                ULTIMA_CELULA.classList.remove('PRIMEIRA');
+
+
+                DESENHAR_BORDA(PARAMETROS_EDITADOS["CELULAS"])
+    
+            }
+        }
+    }
+})
+
+document.body.addEventListener('mouseup', function() {
+    
+    if(MOUSE_PRESSIONADO) {MOUSE_PRESSIONADO = false}
+  
+});
+
+document.addEventListener('keydown', async function (event) {
+     
+    let CELULAS_SELECIONADAS = document.querySelectorAll('.CELULA_SELECIONADA');
+
+    //AO DIGITAR ALGUM NUMERO
+    if (event.key >= '0' && event.key <= '9') {
+      
+      NOVO_VALOR +=  event.key
+
+      CELULAS_SELECIONADAS.forEach(function(elemento) {
+        elemento.innerText = "";
+      });
+
+      CELULAS_SELECIONADAS.forEach(function(elemento) {
+        elemento.innerText = NOVO_VALOR;
+  
+      });
+    }
+
+    //AO APAGAR UM DIGITO
+    if (event.key === 'Backspace') {
+
+        NOVO_VALOR = NOVO_VALOR.slice(0, -1)
+        
+        CELULAS_SELECIONADAS.forEach(function(elemento) {
+            elemento.innerText = "";
+        });
+
+        CELULAS_SELECIONADAS.forEach(function(elemento) {
+            elemento.innerText = NOVO_VALOR;
+        });
+
+    }
+  
+    if (event.key === "Escape") { LIMPAR_SELECOES()  }
+    if ( event.ctrlKey )        { ctrlPressed = true }
+  
+});
+
+document.addEventListener('keyup', function(event) {
+    
+    if (!event.ctrlKey) { ctrlPressed = false }
+
+})
+
+let current_value = 0
+
+document.addEventListener('click', async function(event) {
+
+    let elemento = event.target
+    
+    if (editando_encoste) {
+        
+        elemento = document.querySelector('.input_encoste');
+        tbEdicao = elementoTransformado.voltar_ao_nomral();
+        editando_encoste = false
+    }
+
+    else if (elemento.classList.contains('celula_encoste')) {
+        
+        current_value = elemento.innerText
+        elementoTransformado = new mudarElemento(elemento);
+        tbEdicao = elementoTransformado.para_textBox();
+        tbEdicao.focus();
+        editando_encoste = true
+
+    }
+
+})
